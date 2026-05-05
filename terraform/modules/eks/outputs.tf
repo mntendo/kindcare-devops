a3f50c34ab3eb842862940694578ae17741ae29c
mntendo <mntendo@dons.usfca.edu>
2026-05-05 16:08:11 -0700

    fix: update eks outputs after removing node group

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -13,11 +13,6 @@ output "cluster_certificate_authority" {
   value       = aws_eks_cluster.main.certificate_authority[0].data
 }
 
-output "node_group_name" {
-  description = "Name of the node group"
-  value       = aws_eks_node_group.main.node_group_name
-}
-
 output "cluster_security_group_id" {
   description = "Security group ID attached to the EKS cluster"
   value       = aws_security_group.eks_cluster.id
